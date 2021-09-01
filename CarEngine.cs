e5d02f5a99549f0e51580b8d4fbad7d5e69feb55
KohsukeIde <89821472+KohsukeIde@users.noreply.github.com>
2021-09-01 17:36:31 +0900

    Add files via upload
    A fundamental part of the game

---
+++ CarEngine.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarEngine : MonoBehaviour
+{
+    public Transform path;
+    public float maxSteerAngle = 45f;
+    public WheelCollider wheelFL;
+    public WheelCollider wheelFR;
+    public float distanceFromNode;
+
+    [Header("Sensors")]
+    public float sensorLength = 3f;
+    public float frontSensorPos = 0.5f;
+    public float frontsideSensorPos = 0.2f;
+
+    private List<Transform> nodes;
+    private int currentNode = 0;
+
+    private void Start()
+    {
+        Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for(int i = 0; i < pathTransforms.Length; i++)
+        {
+            if(pathTransforms[i] != path.transform)
+            {
+                nodes.Add(pathTransforms[i]);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Sensors();
+        ApplySteer();
+        Drive();
+        CheckWaypointDistance();
+    }
+
+    private void Sensors()
+    {
+        RaycastHit hit;
+        Vector3 sensorStartPos = transform.position;
+        sensorStartPos.z += frontSensorPos;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+
+        //front center sensor
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+
+        }
+        Debug.DrawLine(sensorStartPos, hit.point);
+
+        //front right sensor
+        sensorStartPos.x += frontsideSensorPos;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+
+        }
+        Debug.DrawLine(sensorStartPos, hit.point);
+
+        //front left sensor
+        sensorStartPos.y -= 2 * frontsideSensorPos;
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+
+        }
+        Debug.DrawLine(sensorStartPos, hit.point);
+
+
+        if (Physics.Raycast(sensorStartPos, fwd, out hit, sensorLength))
+        {
+
+        }
+        Debug.DrawLine(sensorStartPos, hit.point, Color.green);
+
+    }
+
+    private void ApplySteer()
+    {
+        Vector3 relativeVector = this.transform.InverseTransformPoint(nodes[currentNode].position);
+        float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+        wheelFL.steerAngle = newSteer;
+        wheelFR.steerAngle = newSteer;
+    }
+
+    private void Drive()
+    {
+        wheelFL.motorTorque = 1500f;
+        wheelFR.motorTorque = 1500f;
+    }
+
+    private void CheckWaypointDistance()
+    {
+        if (Vector3.Distance(this.transform.position, nodes[currentNode].position) < distanceFromNode)
+        {
+            if(currentNode == nodes.Count - 1)
+            {
+                currentNode = 0;
+            }
+            else
+            {
+                currentNode++;
+            }
+        }
+    }
+
+}
+
---
+++ CarMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CarMove : MonoBehaviour
+{
+    private float speed;
+    private Vector3 roat;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        roat = Vector3.zero;
+        speed = 0;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            speed += (10 - speed) * Time.deltaTime;
+        }
+        else
+        {
+            speed += -speed * Time.deltaTime;
+        }
+        float handle = Input.GetAxis("Horizontal");
+        roat.y += handle * 50 * Time.deltaTime;
+        if (handle > 360)
+            handle -= 360;
+        else if (handle < 0)
+            handle += 360;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.up * -1, out hit, 1.5f))
+        {
+            Quaternion look = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward,
+      hit.normal), hit.normal);
+            look = Quaternion.RotateTowards(transform.rotation, look, 90 * Time.deltaTime);
+            roat.x = look.eulerAngles.x;
+            roat.z = look.eulerAngles.z;
+        }
+        rb.velocity = transform.forward * speed;
+        rb.rotation = Quaternion.Euler(roat.x, roat.y, roat.z);
+    }
+}
